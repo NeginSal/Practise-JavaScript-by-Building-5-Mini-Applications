b64fed93eaf4e37fe358cb23d4c83cc348b09ad6
NeginSal <neginsaljooghi@gmail.com>
2021-04-20 19:44:43 +0430

    element value

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const button = document.querySelector("button");
 const output = document.querySelector(".output");
+
+
 console.log(button);
 button.addEventListener("click",function(){
-    console.log("click");
+    const cost = document.querySelector("input");
+    console.log(cost.value);
     output.innerText="Hi";
+
 })
