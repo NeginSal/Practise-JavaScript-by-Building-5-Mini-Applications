a206311e7a9eee7b2d004d89b5766533e50f5286
NeginSal <neginsaljooghi@gmail.com>
2021-04-20 19:22:30 +0430

    adding event listener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const button = document.querySelector("button")
-console.log(button)
-button.innerHTML="Hello";
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+console.log(button);
+button.addEventListener("click",function(){
+    console.log("click");
+    output.innerText="Hi";
+})
