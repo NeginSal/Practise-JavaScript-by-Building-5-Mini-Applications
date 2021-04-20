ecde80febbfa349d54052c9f476925b870e29fc0
NeginSal <neginsaljooghi@gmail.com>
2021-04-20 20:10:49 +0430

    working with output

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const button = document.querySelector("button");
 const output = document.querySelector(".output");
-
-
 console.log(button);
+
 button.addEventListener("click",function(){
     const cost = document.querySelector("input");
-    console.log(cost.value);
-    output.innerText="Hi";
-
+    let tip =(cost.value);
+    let temp = 'you should pay'+tip+'on$'+cost.value;
+    output.innerHTML= temp;
 })
